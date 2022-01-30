e08f18ef5747e072e47b9e3b2cfce6b2ca17ef4a
Joel Tanig <joeltanig77@gmail.com>
2022-01-30 12:22:59 -0800

    Added some scalable assets and added some file structure that still needs to be fixed

--- src/engine/main.py
+++ src/engine/main.py
@@ -1,4 +1,10 @@
 import pygame
+import math
+
+"""
+    About this file:
+    This is a file that will be the runner of the application
+"""
 import time
 
 import src.engine.scenecreator.sceneCreator as sceneCreator
@@ -6,27 +12,30 @@ import src.engine.scenecreator.sceneCreator as sceneCreator
 def main():
     print("Welcome to the Mammon Engine!")
     pygame.init()
+    scale = 1 #Sets the scale of ALL png's
+    pygame.display.set_caption("Mammon-Engine")
     #For resize use second arg: pygame.RESIZABLE
     mainWindow=pygame.display.set_mode((512,448))
     isRunning = True
     testImg = pygame.image.load("../../data/assets/sprites/testSprite.png")
     grndSprite1 = pygame.image.load("../../data/assets/sprites/groundSprite1.png")
-    testImg = pygame.transform.scale(testImg, (64, 64))
+    testImg = pygame.transform.scale(testImg, ((testImg.get_width()*2)*scale, (testImg.get_height()*2)*scale))
     grndSprite1 = pygame.transform.scale(grndSprite1, (64, 64))
 
     ground=[]
     mainWindow.fill((255, 255, 255))
 
-    mainWindow.blit(testImg, (0, 0))
 
 
     w, h = pygame.display.get_surface().get_size()
 
     #mainWindow.blit(grndSprite1, (224, 224))
+    #This draws the vertical and horizontal bars
     mainWindow=sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512/64, 64,0,h, True)
     mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, 0, h, False)
     mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, 0, 0, False)
     mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, w, h, True)
+    mainWindow.blit(testImg, (0, 0)) #This draws the person
 
     pygame.display.update()
     while(isRunning):
---
+++ src/engine/physics/physics.py
@@ -0,0 +1,11 @@
+# This is Joel Tanig's file
+"""
+    About this file:
+    this is a scratch file that WILL BE REFACTORED LATER into multiple files for physics
+"""
+import pygame
+import math
+
+
+class Physics:
+    pass
