7e6376ca57579eb83a51a412d96e79e85b063637
Coffeemancer99 <trombonedesi@gmail.com>
2022-01-30 20:06:37 -0800

    Updated scenecreator to support vertical and horizontal platforms. Can take dynamic sprite resolutions

--- src/engine/main.py
+++ src/engine/main.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 
-import scenecreator.sceneCreator as sceneCreator
+import src.engine.scenecreator.sceneCreator as sceneCreator
 
 def main():
     print("Welcome to the Mammon Engine!")
@@ -19,18 +19,18 @@ def main():
 
     mainWindow.blit(testImg, (0, 0))
 
+
+    w, h = pygame.display.get_surface().get_size()
+
     #mainWindow.blit(grndSprite1, (224, 224))
-    mainWindow=sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512/64, 64)
+    mainWindow=sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512/64, 64,0,h, True)
+    mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, 0, h, False)
+    mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, 0, 0, False)
+    mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, w, h, True)
 
     pygame.display.update()
     while(isRunning):
-
-
-
         for event in pygame.event.get():
-
-
-
             if event.type == pygame.QUIT:
                 isRunning=False
 
--- src/engine/scenecreator/sceneCreator.py
+++ src/engine/scenecreator/sceneCreator.py
@@ -7,15 +7,19 @@ from src.engine import physics
 #This function creates a straight line of sprites.
 #Window = The current pygame window, sprite = the sprite to paint
 #numBlocks = the number of blocks to paint, spriteSize = the size of the sprite (one side)
+#Start X/Start Y = The position to start
+#up = Boolean, true is vertical platform false is horizontal
+def createPlatform(window, sprite, ground, numBlocks, spriteSize, startX, startY, up):
 
-def createPlatform(window, sprite, ground, numBlocks, spriteSize):
-    w, h = pygame.display.get_surface().get_size()
     start = 0
     while(start<numBlocks):
-        window.blit(sprite, (0+start*spriteSize,h-spriteSize))      # Location of the block, store!!
+        if(up):
+            window.blit(sprite, (startX, startY - (start * spriteSize)))
+        else:
+            window.blit(sprite, (startX + (start * spriteSize), startY - spriteSize))
         start+=1
-
     return window
+
 
 
 # ground can be a list i append objs to
