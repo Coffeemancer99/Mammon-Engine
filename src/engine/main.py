84cfa010bc081a2abc5dc57f313eb33cbef1f912
Coffeemancer99 <trombonedesi@gmail.com>
2022-01-30 17:49:13 -0800

    Added basic sprite testing and window changes.

--- src/engine/main.py
+++ src/engine/main.py
@@ -6,6 +6,10 @@ def main():
     pygame.init()
     mainWindow=pygame.display.set_mode((720,720))
     isRunning = True
+    testImg = pygame.image.load("../../data/assets/sprites/testSprite.png")
+    mainWindow.fill((0, 0, 255))
+    mainWindow.blit(testImg, (0,0))
+    pygame.display.update()
     while(isRunning):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
