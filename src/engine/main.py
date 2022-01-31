ff52c9930d945d4327b88e3504d29a78f5cdf12d
Coffeemancer99 <trombonedesi@gmail.com>
2022-01-30 16:54:21 -0800

    Added pygame import and basic window that stays open until the program is closed.

--- src/engine/main.py
+++ src/engine/main.py
@@ -1,6 +1,16 @@
+import pygame
 
 def main():
     print("Welcome to the Mammon Engine!")
+    pygame.init()
+    mainWindow=pygame.display.set_mode((720,720)) #Main window for pygame display
+    isRunning = True
+    while(isRunning):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                isRunning=False
+    print("Goodbye")
+
 
 if(__name__ == "__main__"):
     main()
