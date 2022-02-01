6467f332e4dc7a3fde9d83de88a9894122eff4fc
Coffeemancer99 <trombonedesi@gmail.com>
2022-01-31 19:34:03 -0800

    Added FPS cap as well as make createPlatform() more dynamic. Prepping for entity storage.

--- src/engine/main.py
+++ src/engine/main.py
@@ -12,6 +12,8 @@ import src.engine.scenecreator.sceneCreator as sceneCreator
 def main():
     print("Welcome to the Mammon Engine!")
     pygame.init()
+    clock=pygame.time.Clock() #Clock used for frame rate
+    framerate=60
     scale = 1 #Sets the scale of ALL png's
     pygame.display.set_caption("Mammon-Engine")
     #For resize use second arg: pygame.RESIZABLE
@@ -25,24 +27,32 @@ def main():
     ground=[]
     mainWindow.fill((255, 255, 255))
 
+    mainWindow.blit(testImg, (0, 0))
+
+    background = (255,255,255)
 
 
     w, h = pygame.display.get_surface().get_size()
 
-    #mainWindow.blit(grndSprite1, (224, 224))
     #This draws the vertical and horizontal bars
     mainWindow=sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512/64, 64,0,h, True)
     mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, 0, h, False)
-    mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, 0, 0, False)
-    mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, w, h, True)
-    mainWindow.blit(testImg, (0, 0)) #This draws the person
-
+    mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, 0, 64, False)
+    mainWindow = sceneCreator.createPlatform(mainWindow, grndSprite1, ground, 512 / 64, 64, w-64, h, True)
+    mainWindow.blit(testImg, (0, 0))  # This draws the person
+    changeDetected=False
     pygame.display.update()
+    #DANGEROUS TERRITORY
+    #MAIN GAME LOOP
     while(isRunning):
+        clock.tick(framerate)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 isRunning=False
-
+        time.sleep(1)
+        if(changeDetected):
+            sceneCreator.clearBG(mainWindow, background)
+            changeDetected=False
     print("Goodbye")
 
 
