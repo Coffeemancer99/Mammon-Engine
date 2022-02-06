af2e71cdc2349606de93041fab6a8c9a11e76167
Joel Tanig <joeltanig77@gmail.com>
2022-02-06 15:25:36 -0800

    added very little to physics.py

--- src/engine/physics/physics.py
+++ src/engine/physics/physics.py
@@ -2,10 +2,46 @@
     About this file:
     this is a scratch file that WILL BE REFACTORED LATER into multiple files for physics
 """
+# The box needs a Vector of doubles x and y and another class that implements Vector and has a mass and friction
+# Treat force and acceleration as a 2d vector and treat them as x and y. Gravity = y vector direction = x vector
+
 import pygame
 import math
 import sympy
 
 
-class Physics:
-    pass
+def force(mass, acceleration):
+    return float(mass * acceleration)
+
+
+""" (Normal Force formula)
+
+    Uses the earth's gravity and multiples it by the mass provided 
+    This function needs to be looped so it can update 
+    
+    :param mass: - the mass that needs to be computed for force
+    
+"""
+
+
+def computeMovementForce(mass):
+    return float(mass * -9.81)
+
+
+""" (Friction formula)
+
+    f = mu*N
+    mu = coefficient of friction
+    N = Normal force
+    
+    Note: Must call the force function before you can use friction, and must pass in a known friction coefficient
+    
+    Uses the mass provided to calculate normal force then multiples it by the coefficientOfFriction provided
+    
+    :param mass: - the mass that needs to be provided
+    :param coefficientOfFriction
+"""
+
+
+def friction(mass, coefficientOfFriction):
+    return float(coefficientOfFriction * computeMovementForce(mass))
