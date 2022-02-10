f96e3d8b99989725b451bde48e3e388d05a59f2d
Coffeemancer99 <trombonedesi@gmail.com>
2022-02-09 18:40:51 -0800

    Implemented player controller in testgame.py file. Tilemaps support collisions and work well with player controller on the y axis. X axis TBD

--- src/engine/menus/testgame.py
+++ src/engine/menus/testgame.py
@@ -1,5 +1,6 @@
 import src.engine.scenecreator.drawTileMap as drawTileMap
 import src.engine.scenecreator.tile as tile
+import src.engine.player.playerController as player
 import pygame
 def getGameMap():
     L=[
@@ -29,26 +30,38 @@ def startGame(mainWindow, scale, framerate):
     groundSprite = pygame.image.load("../../data/assets/sprites/groundSprite1.png")
     groundSprite = pygame.transform.scale(groundSprite, ((groundSprite.get_width()) * scale, (groundSprite.get_height()) * scale))
 
+    brian = player.Player(300,150)
+
     images = [None, groundSprite]
     currMap = getGameMap()
     staticTiles=drawTileMap.drawScene(mainWindow, currMap, images)
     for tile in staticTiles:
         print(tile.rectCol.x, tile.rectCol.y)
     isRunning=True
+
     while(isRunning):
         clock.tick(framerate)
+        brian.update()
+        isCollided = False
+        tile = drawTileMap.drawScene(mainWindow, currMap, images)
+        mainWindow.blit(brian.sprite, (brian.rect.x, brian.rect.y))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 isRunning=False
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                mainWindow.blit(groundSprite, pos)
-                rect=groundSprite.get_rect()
-                rect.x=pos[0]
-                rect.y=pos[1]
-                #print(rect.x)
-                for tile in staticTiles:
-                    if tile.rectCol.colliderect(rect):
-                        print("COLLISION")
-                        pass
+            # if event.type == pygame.MOUSEBUTTONUP:
+                # pos = pygame.mouse.get_pos()
+                # mainWindow.blit(groundSprite, pos)
+                # rect=groundSprite.get_rect()
+                # rect.x=pos[0]
+                # rect.y=pos[1]
+                # #print(rect.x)
+        for tile in staticTiles:
+            if tile.rectCol.colliderect(brian.rect):
+                isCollided=True
+                print("collision")
+                brian.isGrounded=True
+                #brian.rect.y=tile.rectCol.top-33
+            if isCollided==False:
+                brian.isGrounded=False
+
         pygame.display.update()
--- src/engine/player/playerController.py
+++ src/engine/player/playerController.py
@@ -1,23 +1,26 @@
 import pygame
-import esper
+#import esper
 
 class Player():
     def __init__(self, x, y):
-        sprite = pygame.image.load("../../data/assets/sprites/groundSprite1.png")
+        self.sprite = pygame.image.load("../../data/assets/sprites/bolSprite.png")
         # may need to scale later
         self.rect = self.sprite.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.velY = 0
         self.jumped = False
+        self.isGrounded=False
 
     def update(self):
+
         curX = 0
         curY = 0
 
         key = pygame.key.get_pressed()
-        if key[pygame.K_UP] and self.jumped == False:
-            self.velY = -12  # negative moves up
+        if key[pygame.K_UP] and self.jumped == False and self.isGrounded:
+            self.velY = -16  # negative moves up
+            self.isGrounded = False
             self.jumped = True
         if key[pygame.K_UP] == False:
             self.jumped = False
@@ -27,13 +30,19 @@ class Player():
             curX += 4
 
         # add some form of gravity
-        self.velY += 1
-        if(self.velY > 12):
-            self.velY = 12
+        if self.isGrounded:
+            self.velY = 0
+        else:
+            self.velY += 1
+            if(self.velY > 6):
+                self.velY = 6
 
         # make sure to set curY
         curY += self.velY
 
         # update coords
-        self.rect.x = curX
-        self.rect.y = curY
+        self.rect.x += curX
+        # if self.isGrounded:
+        #     self.rect.y +=0
+        # else:
+        self.rect.y += curY
