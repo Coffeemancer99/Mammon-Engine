96434f0a2fec8811c2c6eb4656ecedf39d01b294
drakeworkes@gmail.com <Smu6vNRp5VcNV2V>
2022-02-28 01:11:56 -0800

    >>minigameManager.py - Began main loop logic - early minigame file detection

--- src/game/boardGame/minigameManager.py
+++ src/game/boardGame/minigameManager.py
@@ -1,11 +1,49 @@
 #Authored by Drake Farmer
 
+import random
+import src.minigame as minigames
+import pygame
+import os
+
+# Calling runMinigame will display a rotating list of minigames, select one, and run it
+# Once a minigame has ended it returns the scores and runMinigame will reward players
+
+# IMPORTANT!
+    # Minigame return is expected to be an array of length 4
+
+    # [p1Winnings, p2Winnings, p3Winnings, p4Winnings, item]
+
+        # p1 winnings - p4Winnings is how much p1 won from the minigame. Accepts positive and negative ints
+        # item is only used during duel minigames. Any other minigames should return "None" here!
+
+
+def runMinigame(mainWindow, scale, framerate):
+    clock = pygame.time.Clock()
+    isRunning = True
+    gameList = []
+    # Do once:
+        # Get minigames, save to array
+        # Draw background rectangle
+        # initial draw of first 3 minigames on rectangle
+        # Create array of len 2 to hold 3 minigames on "wheel"
+    for (dirs) in os.walk(minigames):
+        for name in dirs:
+            gameList.append(name)
+
+    print("Minigames found:")
+    for (names) in gameList:
+        print(names + " | ")
+
+
+
+    while (isRunning):
+        clock.tick(framerate)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                isRunning = False
+
 #Set up secondary game loop
-                        #Do once:
-                            #Get minigames, save to array
-                            #Draw background rectangle
-                            #initial draw of first 3 minigames on rectangle
-                            #Create array of len 2 to hold 3 minigames on "wheel"
+
                         #For loop
                             #grow array[0] slightly and move down
                             #move array[1] down slightly
