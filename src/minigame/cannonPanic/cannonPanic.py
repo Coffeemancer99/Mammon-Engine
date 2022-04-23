ba207fc58fe476618f2921333061e0123067e2f9
daniel.c.lawson <daniel.c.lawson@wsu.edu>
2022-04-23 13:53:03 -0700

    started improvements to cannonPanic (probably currently broken)

--- src/minigame/cannonPanic/cannonPanic.py
+++ src/minigame/cannonPanic/cannonPanic.py
@@ -5,6 +5,9 @@ import src.minigame.fruitPanic.handController as player
 import pygame
 import time as time
 import src.minigame.teamMasher.masher as masher
+
+import src.minigame.physicsTest.ball as ball
+
 class cannon():
     def __init__(self):
         self.loaded = None
@@ -28,9 +31,25 @@ class cannonLoader():
     def placeBall(self, cannon):
         pass
 
-class cannonBall():
-    def __init__(self, x, y):
-        pass
+class cannonBall(ball.Ball):
+    def __init__(self, sprite, scale, x, y, lifetime, timerEnabled, name="undefinedCanBall", mass = 10, maxpower = defMaxPow):
+        ball.Ball.__init__(self, sprite, scale, x, y, name, mass, maxpower)
+        self.lifetime = lifetime
+        self.durTime = timer(3, 60)
+        self.alive = True
+        self.timerEnabled = timerEnabled
+
+class Ball(DynamicObject):
+    def __init__(self, sprite, scale, x, y, lifetime, timerEnabled, name="undefinedBall", mass = 10, maxpower = defMaxPow):
+        DynamicObject.__init__(self, sprite, scale, x, y, name, mass)
+        self.power = 0
+        self.angle = 0
+        self.maxpower = maxpower
+        self.lifetime = lifetime
+        self.durTimer = timer(3, 60)
+        self.alive = True
+        self.timerEnabled = timerEnabled
+
 """
 import random
 import src.minigame.fruitPanic.fruit as fruit
--- src/minigame/cannonPanic/cannonball.py
+++ src/minigame/cannonPanic/cannonball.py
@@ -2,41 +2,24 @@ import pygame
 import math
 import sys # for flushing stdout for dev statements, remove later
 import src.engine.physics.physics as physics
-from src.engine.physics.physics import Object
-from src.engine.physics.physics import DynamicObject
+from src.engine.physics.physics import Object, DynamicObject, RectObject, DynamicRect
 import src.minigame.physicsTest.ball as ball
+from src.minigame.physicsTest.ball import Ball
 from src.minigame.timer.timer import timer as timer
 
 defMaxPow = 80
-class CannonBall(ball.Ball):
-    def __init__(self, sprite, scale, x, y, lifetime, timerEnabled, name="undefinedBall", mass=10, maxpower=defMaxPow):
-        ball.Ball.__init__(self, sprite, scale, x, y, name="undefinedBall", mass=10,
+class CannonBall(Ball):
+    def __init__(self, sprite, scale, x, y, lifetime, timerEnabled, name="undefinedC-Ball", mass=10, maxpower= ball.defMaxPow):
+        Ball.__init__(self, sprite, scale, x, y, name="undefinedBall", mass=10,
                            maxpower=defMaxPow)
         self.lifetime = lifetime
         self.durTimer = timer(3, 60)
         self.alive = True
         self.timerEnabled = timerEnabled
 
-
-
-    def launch(self):
-        if(self.power == 0):
-            return
-        print("\r\rlaunched!            \n")
-        print("power = " + str(self.power))
-        print("angle = " + str(self.angle) + " = " + str(self.angle * 180 / math.pi))
-        print("powX = " + str(self.power*math.cos(self.angle)))
-        print("powY = " + str(self.power*math.sin(self.angle)))
-        print("")
-
-
-        self.momX += self.power*math.cos(self.angle)*2*self.scale
-        self.momY -= self.power*math.sin(self.angle)*2*self.scale
-        self.power = 30
-        #self.angle = 0
-
     def timeUntilDeletion(self):
         self.durTimer.decrement()
         if(self.timerEnabled and self.durTimer.isFinished()):
-
             self.alive = False
+    def launch(self, resetAngle = False, resetPower = False):
+        Ball.launch(self, resetAngle, resetPower)
